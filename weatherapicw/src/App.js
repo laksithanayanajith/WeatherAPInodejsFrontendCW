fdc9919a47c87dc5f16dbc8664be7416a5b4837c
laksithanayanajith <lakshitha.nayanajith@app360.lk>
2024-04-04 15:34:11 +0530

    added sri lanka live map

--- weatherapicw/src/App.js
+++ weatherapicw/src/App.js
@@ -1,20 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import WeatherDataDisplay from './WeatherDataDisplay';
+import React, { useState } from "react";
+import "./App.css";
+import WeatherFetcher from "./WeatherFetcher"; // Importing WeatherFetcher component
+import MapContainer from "./components/map_container";
 
 const App = () => {
-  const [weatherData, setWeatherData] = useState([]);
-
-  useEffect(() => {
-    // Fetch weather data from your API
-    fetch('http://localhost:3002/api/weather')
-      .then(response => response.json())
-      .then(data => setWeatherData(data.data))
-      .catch(error => console.error('Error fetching weather data:', error));
-  }, []);
+  const [setWeatherData] = useState([]);
 
   return (
-    <div>
-      <WeatherDataDisplay weatherData={weatherData} />
+    <div className="container" style={{display: "flex", flexDirection:"column",padding: "20px", justifyContent: "center", alignItems:"center", gap: "20px"}}>
+            {/* <WeatherFetcher setWeatherData={setWeatherData} />
+            <WeatherFetcherByDistrict setWeatherData={setWeatherData} /> */}
+            <MapContainer />
+            <WeatherFetcher setWeatherData={setWeatherData} />
     </div>
   );
 };
