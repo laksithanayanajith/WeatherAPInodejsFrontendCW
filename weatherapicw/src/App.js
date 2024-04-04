d072455853095f9abfa4d54c4c3b9d1e1a516f1e
Laksitha Nayanajith <80520311+laksithanayanajith@users.noreply.github.com>
2024-04-04 20:24:41 +0530

    Update App.js
    updated context

--- weatherapicw/src/App.js
+++ weatherapicw/src/App.js
@@ -6,14 +6,6 @@ import MapContainer from "./components/map_container";
 const App = () => {
   const [weatherData, setWeatherData] = useState([]);
 
-  useEffect(() => {
-    // Fetch weather data from your API
-    fetch('api/weather')
-      .then(response => response.json())
-      .then(data => setWeatherData(data.data))
-      .catch(error => console.error('Error fetching weather data:', error));
-  }, []);
-
   return (
     <div className="container" style={{display: "flex", flexDirection:"column",padding: "20px", justifyContent: "center", alignItems:"center", gap: "20px"}}>
             {/* <WeatherFetcher setWeatherData={setWeatherData} />
