0826f66d7d8a6845fb2df156d85afc7ad1f03e4a
Laksitha Nayanajith <80520311+laksithanayanajith@users.noreply.github.com>
2024-04-04 20:27:05 +0530

    Update WeatherFetcherByDistrict.js
    added api access key

--- weatherapicw/src/WeatherFetcherByDistrict.js
+++ weatherapicw/src/WeatherFetcherByDistrict.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
-import WeatherDataDisplayByDistrict from './WeatherDataDisplayByDistrict'; // Import WeatherDataDisplayByDistrict component
+import WeatherDataDisplayByDistrict from './WeatherDataDisplayByDistrict';
 
 const WeatherFetcherByDistrict = () => {
   const [weatherData, setWeatherData] = useState([]);
 
   const fetchDataByDistrict = () => {
+    const apiKey = 'xvkffkvkrsvmtormbgdttjdcjgfskmsrhfsrmg';
     const districtInput = prompt('Enter the district:');
     if (districtInput) {
-      fetch(`https://weatherapinodejsbackendcw.onrender.com/api/Weather/byDistrict/${districtInput}`)
+      fetch(`https://weatherapinodejsbackendcw.onrender.com/api/Weather/byDistrict/${districtInput}`, {
+        headers: {
+          'x-api-key': apiKey
+        }
+      })
         .then(response => response.json())
         .then(data => setWeatherData(data.data))
         .catch(error => console.error('Error fetching weather data by district:', error));
